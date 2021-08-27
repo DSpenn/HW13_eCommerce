a90e6fedfdab0a57e60cb976cc0b150d2860fb7b
DS <derekstander@gmail.com>
2021-08-27 17:46:49 -0400

    modified:   Develop/models/index.js  started working on models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+});
 // Categories have many Products
+Category.hasmany(Product , {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
